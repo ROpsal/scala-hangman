32a273a3a2e9943f894f7d1d70f68062ac24e403
Richard Opsal <gegenteil@fastmail.com>
2023-02-04 19:35:39 -0600

    Reorganized to more functional approach.

--- src/main/scala/Hangman.scala
+++ src/main/scala/Hangman.scala
@@ -1,138 +1,175 @@
 /**
  * Author: Richard B. Opsal, Ph.D.
- * Update: 2015/08/21
+ * Update: 2023/02/04
 **/
+
 
 ///
 // Helper functions for Hangman application.
 ///
 
-package object hm {
+object Helpers {
 
   // Generate list of possible words from passed file.
+  // Empty list means nothing was loaded.
   def wordList(fname : String = "dictionaryWords.txt") : List[String] = {
-    val source = io.Source.fromFile( fname )
-    val words : List[String] = source.getLines.toList
-    source.close()
-    words
+    scala.util.Using(io.Source.fromFile( fname )){ source =>
+      source.getLines.toList
+    }.fold(_ => List.empty[String], identity)
   }
 
   // Return a random word from the passed list.
-  def randomWord(words : List[String]) : String = {
+  extension (words : List[String]) def randomWord : String = {
     words( scala.util.Random.nextInt(words.length) )
   }
 
-  // Split the word into individual letters.
-  def wordSplit(word : String) : List[Char] = {
-    word.toList
+  // Split the word into individual letters plus upper case letters.
+  extension (word : String) def splitWordToUpper : List[Char] = {
+    word.toUpperCase.toList
   }
 
   // Join the list of characters together with a space in-between.
-  def wordJoin(wordlist : List[Char]) : String = {
+  extension (wordlist : List[Char]) def joinWord : String = {
     wordlist.mkString(" ")
   }
 
   // Set of upper case letters.
-  def alphaSet : Set[Char] = {
-    ('A' to 'Z').toSet
+  val alphaSet : Set[Char] = ('A' to 'Z').toSet
+}
+
+
+///
+// State case classes and associated routines.
+///
+
+object State {
+  import Helpers.*
+
+  case class Words(hangWord: String, words: List[String]) {
+    def nextWord: Words = {
+      val newHangWord = this.words.randomWord
+      Words(newHangWord, this.words.filterNot(_ == newHangWord))
+    }
   }
 
-  // Generate a new guess list based on letter, current matches and actual word.
-  def applyGuess(letter : Char, guesslist : List[Char], hanglist : List[Char]) : List[Char] = {
-    guesslist.zip(hanglist).map({ case (g, h) => if (letter == h) h else g })
+  case class WinsAndLosses(wins: Int = 0, losses: Int = 0) {
+    def addLoss: WinsAndLosses = this.copy(losses = this.losses + 1)
+    def  addWin: WinsAndLosses = this.copy(wins = this.wins + 1)
+  }
+
+  enum Status {
+    case Win, Loss, Match, NoMatch, NoChange
+  }
+
+  private val maxGuesses = 6
+  case class Guess(hangList: List[Char], guessList: List[Char], guessSet: Set[Char], guesses: Int) {
+    // Input letter assumed to be uppercase.
+    import Status.*, Guess.applyGuess
+    def nextGuess: Char => (Status, Guess) = { letter =>
+      if (guessSet.contains(letter)) {
+        (NoChange, this)
+      } else {
+        val `match` = this.hangList.contains(letter)
+        val newGuess = this.copy(
+          guessList = if `match` then letter.applyGuess(this.guessList, this.hangList) else this.guessList,
+          guessSet = this.guessSet + letter,
+          guesses = if `match` then this.guesses else if 0 < this.guesses then this.guesses - 1 else 0
+        )
+        val status =
+          if newGuess.hangList == newGuess.guessList then Win
+          else if 0 == newGuess.guesses then Loss
+          else if `match` then Match
+          else NoMatch
+        (status, newGuess)
+      }
+    }
+
+    def finishGuess: WinsAndLosses => WinsAndLosses = { winsAndLosses =>
+      if this.guesses == maxGuesses then winsAndLosses else winsAndLosses.addLoss
+    }
+  }
+
+  object Guess {
+    extension (letter: Char) def applyGuess(guessList: List[Char], hangList: List[Char]): List[Char] = {
+      guessList.zip(hangList).map((g, h) => if (letter == h) h else g)
+    }
+
+    val nextRound: Words => Guess = words => {
+      Guess(
+        hangList = words.hangWord.splitWordToUpper,
+        guessList = List.fill[Char](words.hangWord.length)('_'),
+        guessSet = Set.empty[Char],
+        guesses = maxGuesses
+      )
+    }
   }
 }
+
 
 ///
 // The Hangman application.
 ///
 
-@main def Hangman(args: String*): Unit = {
+@main def Hangman (args: String*): Unit = {
 
-  // Cheery intro to the Hangman game.
-  println("Welcome to the Hangman word guessing game.")
-
-  // List of words to guess from.
-  val fname = if (args.isEmpty) "src/resources/dictionaryWords.txt" else args(0)
-  var words = hm.wordList(fname)
-
-  // Allowable set of characters to guess from.
-  val alphaset : Set[Char] = hm.alphaSet
+  import Helpers.*
+  import State.*, State.Status.*
+  import scala.io.StdIn.readLine
 
   // Formatting strings for nice output.
   val fmtsummary = "%s  Wins : %2d  Losses : %2d"
   val fmtinput   = "\t%s  [Guesses left : %2d ] Letter : "
 
-  // The game play loop.
-  var fexit = false
-  var wins   = 0
-  var losses = 0
-  while (!fexit) {
-    println("Type 'Exit' to leave the game, 'New' for a new game.")
-    println("Good luck!\n")
+  def printSummary(message: String, hangList: List[Char], winsAndLosses: WinsAndLosses): Unit = {
+    println("\t" + hangList.joinWord + "\n")
+    println(fmtsummary.format(message, winsAndLosses.wins, winsAndLosses.losses))
+    println()
+  }
 
-    val hangword = hm.randomWord(words)
-    words = words.filterNot(_ == hangword)
-    val hanglist = hm.wordSplit(hangword.toUpperCase)
-
-    var guesslist = hm.wordSplit("_" * hangword.length)
-    var guesset : Set[Char] = Set()
-    var guesses = 6
-
-    var fnewgame = false
-    while (!fnewgame && !fexit) {
-
-      def checkGuess() : Unit = {
-
-        def printSummary(message : String) : Unit = {
-          println("\t" + hm.wordJoin(hanglist) + "\n")
-          println(fmtsummary.format(message, wins, losses))
-        }
-
-        if (hanglist == guesslist) {
-          fnewgame = true
-          wins += 1
-          printSummary("Congratulations on your win!")
-
-        } else {
-          guesses match {
-            case 1 =>
-              fnewgame = true
-              losses += 1
-              printSummary("Too Bad! Please try again.")
-            case _ => guesses -= 1
-          }
-        }
-      }
-
-      val line = scala.io.StdIn.readLine(fmtinput.format(hm.wordJoin(guesslist), guesses)).toUpperCase
-//    println(line)
-      line match {
-        case "NEW"  => guesses = 1 ; checkGuess()
-        case "EXIT" => fexit = true
-        case "" => Nil
-        case _ => {
-          val letter : List[Char] = line.toList
-          if ((1 < letter.length) || !alphaset.contains(letter.head)) {
-            // An unexpected entry.
-            println("Not a valid guess -> " + line)
-
-          } else if (guesset.contains(letter.head)) {
-            // Ignore this case. Already guessed this letter.
+  def enterGameR(words: Words, winsAndLosses: WinsAndLosses): Unit = {
+    def playGameR(guess: Guess): (Boolean, WinsAndLosses) = {
+      readLine(fmtinput.format(guess.guessList.joinWord, guess.guesses)).toUpperCase match {
+        case "NEW" => (false, guess.finishGuess(winsAndLosses))
+        case "EXIT" => (true, guess.finishGuess(winsAndLosses))
+        case str: String if str.isEmpty => playGameR(guess)
+        case entry: String => {
+          val letter = entry.head
+          if ((1 < entry.length) || !alphaSet.contains(letter)) {
+            println(s"Not a valid guess -> $entry")
+            playGameR(guess)
 
           } else {
-            if (hanglist.contains(letter.head)) {
-              guesslist = hm.applyGuess(letter.head, guesslist, hanglist)
-              guesses += 1
-            }
+            val (status, newGuess) = guess.nextGuess(letter)
+            status match {
+              case Win  =>
+                val newWinsAndLosses = winsAndLosses.addWin
+                printSummary("Congratulations on your win!", guess.hangList, newWinsAndLosses) ;
+                (false, newWinsAndLosses)
 
-            guesset = guesset + letter.head
-            checkGuess()
+              case Loss =>
+                val newWinsAndLosses = winsAndLosses.addLoss
+                printSummary("Too Bad! Please try again.", guess.hangList, newWinsAndLosses) ;
+                (false, newWinsAndLosses)
+
+              case _ => playGameR(newGuess)
+            }
           }
         }
       }
     }
+
+    val (fDone, newWinsAndLosses) = playGameR(Guess.nextRound(words))
+    println()
+    if !fDone then enterGameR(words.nextWord, newWinsAndLosses)
   }
 
-  println("Thank you for playing Scala Hangman!")
+  // List of words to guess from.
+  val fname = if args.isEmpty then "src/resources/dictionaryWords.txt" else args(0)
+  val words = Words("HANGMAN", Helpers.wordList(fname)).nextWord
+
+  println("Welcome to the Hangman word guessing game.")
+  println("Type 'Exit' to leave the game, 'New' for a new game.")
+  println("Good luck!\n")
+  enterGameR(words, WinsAndLosses())
+  println("\nThank you for playing Scala Hangman!")
 }
